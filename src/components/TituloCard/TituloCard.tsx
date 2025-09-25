90cfaeed521887fb1e2e5063772c082ddb5c7e9c
devseravali <devseravali@gmail.com>
2025-09-25 18:11:23 -0300

    Criação de componente de TituloCard

---
+++ src/components/TituloCard/TituloCard.tsx
@@ -0,0 +1,21 @@
+import type { TituloCard as TituloCardType } from '../../types/types';
+import { useMouseOver } from '../../hooks/useMouseOver';
+
+export const TituloCard = ({ titulo }: TituloCardType) => {
+  const { isHovering, onMouseEnter, onMouseLeave } = useMouseOver();
+  return (
+    <p
+      className={`
+        text-center text-[30px] mb-5 font-extrabold cursor-pointer
+        text-[var(--destaque-escuro)]
+        transition-colors duration-300
+        hover:text-[var(--hover-destaque-escuro)] 
+        ${isHovering ? "uppercase" : ""}
+      `}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      { isHovering ? titulo.toUpperCase() : titulo}
+    </p>
+  );
+};
