422ad5e8575a1a75ab55f38983bb5d4d12b9336f
devseravali <devseravali@gmail.com>
2025-09-25 18:15:33 -0300

    Adiciona dados iniciais do personagem Okarun

---
+++ src/data/okarun.ts
@@ -0,0 +1,29 @@
+export const okarun = {
+  title: { 
+    titulo: "dan da dan"
+  },
+  image: {
+    src: "/images/okarun-icon.jpg",
+    alt: "Foto do personagem Okarun",
+  },
+  nome: {
+    name: "Okarun",
+  },
+  sobre: {
+    age: "16 Anos",
+    personality:
+      "Okarun é um jovem nerd e introvertido, mas também altruísta e gentil, que valoriza profundamente sua amizade com Momo Ayase.",
+    objective:
+      "Seu principal objetivo é recuperar seus testículos, que foram roubados pela Vovó Turbo, e ficar mais forte para proteger a si mesmo e a Momo.",
+    ability:
+      "Ken “Okarun” Takakura, de Dandadan, combina força e agilidade sobre-humanas, percepção de espíritos, acesso a dimensões espirituais, teletransporte por fios e projeção astral, potencializados pelos poderes da Vovó Turbo.",
+  },
+  curiosidade: {
+    title: "Curiosidade",
+    curiosities: ["Takura é um jovem confiante e disciplinado, mas também leal e justo, que valoriza profundamente sua amizade e alianças com os colegas de equipe. Seu principal objetivo é se tornar mais poderoso, superar inimigos formidáveis e proteger a si mesmo e aqueles que considera importantes. Ken “Takura”, de Dan Da Dan, combina força e agilidade sobre-humanas, resistência extraordinária, percepção estratégica, habilidades de combate avançadas, teletransporte tático e técnicas de projeção, potencializadas por seu treinamento rigoroso e determinação."
+  ]
+  },
+  button: {
+    label: "Saiba Mais",
+  },
+};
