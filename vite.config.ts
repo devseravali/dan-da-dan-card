fff873622370bc28d73711d353ae518263f6ed68
devseravali <devseravali@gmail.com>
2025-09-25 19:19:37 -0300

    Melhoria no projeto

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  base: '/dan-da-dan-card/', 
 })
