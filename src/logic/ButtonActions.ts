2a2d878389ca9cb5f3a1b6f1b1f92771ba1933c1
devseravali <devseravali@gmail.com>
2025-09-25 18:16:41 -0300

    Adiciona funções de utilidade para ações de botões

---
+++ src/logic/ButtonActions.ts
@@ -0,0 +1,5 @@
+export const handleClick = () => {
+alert (
+    "Okarun, em Dan-Da-Dan, é um personagem carismático e brincalhão, cuja agilidade, inteligência tática e momentos cômicos contrastam com raras cenas emocionais profundas, tornando-o ao mesmo tempo divertido e estratégico."
+    );
+  };
